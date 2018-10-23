dad2aced62a6c6c58f580b4e2a5568de352b5b99
bkaplan-github <43967620+bkaplan-github@users.noreply.github.com>
2018-10-23 16:26:35 -0400

    Add files via upload

--- dist/main.js
+++ dist/main.js
@@ -66,8 +66,6 @@ console.log("update end");
     }
 
     function addWidget(name, value) {
-      params[name] = {'name': name, 'value': value};
-      
       var widget = '\n<div id="widget-' + name + '" class="slds-form-element" style="margin-bottom:10px;">\n<label class="slds-form-element__label" for="input-id-' + name + '">' + name + '</label>\n<div class="slds-form-element__control slds-input-has-fixed-addon">\n<input class="slds-input" type="text" id="input-id-' + name + '" placeholder="Value" />\n<button id="delete-parameter-button-' + name + '" class="slds-button slds-button_icon slds-button_icon-border-filled" title="Delete Parameter"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="delete" width="100%" height="100%"><path d="M21 4.6h-5.8V2.8c0-1-.8-1.9-1.8-1.9h-2.8c-1 0-1.8.9-1.8 1.9v1.8H3c-.4 0-.7.3-.7.7v1.4c0 .4.3.7.7.7h18c.4 0 .7-.3.7-.7V5.3c0-.4-.3-.7-.7-.7zM10.6 3.2c0-.2.2-.4.5-.4h1.8c.3 0 .5.2.5.4v1.4h-2.8V3.2zm8.6 6H4.8c-.3 0-.6.4-.6.7v10.9c0 1.3 1 2.3 2.3 2.3h11c1.3 0 2.3-1 2.3-2.3V9.9c0-.3-.3-.7-.6-.7zm-8.6 10.2c0 .3-.2.4-.4.4h-1c-.2 0-.4-.1-.4-.4v-6.5c0-.3.2-.4.4-.4h1c.2 0 .4.1.4.4v6.5zm4.6 0c0 .3-.2.4-.4.4h-1c-.2 0-.4-.1-.4-.4v-6.5c0-.3.2-.4.4-.4h1c.2 0 .4.1.4.4v6.5z"></path></svg><span class="slds-assistive-text">Delete Parameter</span></button>\n</div>\n</div>';
       $('#workspace-container').append(widget);
 
@@ -116,9 +114,10 @@ console.log("update end");
 
       // add a new widget
       $('#add-parameter-button').click(function() {
-        var widgetName = $('#add-parameter-name').val();
-        if (widgetName) {
-          addWidget(widgetName, '');
+        var name = $('#add-parameter-name').val();
+        if (name) {
+          params[name] = {'name': name, 'value': ""};
+          addWidget(name, '');
           $('#add-parameter-name').val('');
           console.log("add param");
           updateContent();
@@ -143,11 +142,12 @@ console.log("update end");
           for (var i = 1; i < ampArray.length; i++) {
             var a = ampArray[i];
             var nameEnd = a.indexOf(" ");
-            var p = a.substring(0, nameEnd);
+            var name = a.substring(0, nameEnd);
             var vStart = a.substring(a.indexOf('= "') + 3);
-            var v = vStart.substring(0, vStart.indexOf('"'));
+            var value = vStart.substring(0, vStart.indexOf('"'));
             // console.log(p+"="+v);
-            addWidget(p, v);
+            params[p] = {'name': name, 'value': value};
+            addWidget(name, value);
           }
 
           html = data.substring(paramTextEnd + 26);
