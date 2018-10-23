c3fce8589ee48fc268c694e14603fe7e7e0d88c7
bkaplan-github <43967620+bkaplan-github@users.noreply.github.com>
2018-10-23 16:46:20 -0400

    Add files via upload

--- dist/main.js
+++ dist/main.js
@@ -70,7 +70,7 @@
         // alert($(this).data('id') + ' ' + $(this).val());
         var name = $(this).data('id');
         var value = $(this).val();
-        params[name][value] = value;
+        params[name]['value'] = value;
         updateContent();
       });
 
