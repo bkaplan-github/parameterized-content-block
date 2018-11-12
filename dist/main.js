9e6072feeb1ad393a85cff9e8a9926a36d1207f8
bkaplan-github <43967620+bkaplan-github@users.noreply.github.com>
2018-11-12 16:17:52 -0500

    Add files via upload

--- dist/main.js
+++ dist/main.js
@@ -25,16 +25,6 @@ function debounce (func, wait, immediate) {
 }
 */
 
-/*
-function getParams() {
-  var output = "";
-  for (const param in params) {
-    output += param+"="+params[param]+", ";
-  }
-  return output;
-}
-*/
-
 function escapeRegExp(text) {
   return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 }
@@ -49,7 +39,6 @@ function htmlEscape(str) {
     .replace(/>/g, '&amp;gt;');
 }
 
-// I needed the opposite function today, so adding here too:
 function htmlUnescape(str){
   return str
     .replace(/&amp;quot;/g, '"')
@@ -71,7 +60,8 @@ function updateContent() {
   for (const param in params) {
     var name = params[param]['name'];
     var value = htmlEscape(params[param]['value']);
-    ampscript += '\r\nSET @' + name + ' = TreatAsContent("' + value + '")';
+    var options = params[param]['options'];
+    ampscript += '\r\nSET @' + name + ' = TreatAsContent("' + value + '") /* ' + options + ' */';
     regex = new RegExp(escapeRegExp("%%=v(@"+name+")=%%"), "gi");
     fakehtml = fakehtml.replace(regex, value);
   }
@@ -84,11 +74,36 @@ function updateContent() {
   sdk.setContent(ampscript+"\r\n"+html);
 }
 
-function addWidget(id, name, value) {
+function addWidget(id, name, value, type, options) {
   var regex = new RegExp(escapeRegExp("_"), "gi");
   var title = name.replace(regex, " ");
 
-  var widget = '\n<div id="widget-' + id + '" class="slds-form-element" style="margin-bottom:10px;">\n<label class="slds-form-element__label" for="input-id-' + id + '">' + title + '</label>\n<div class="slds-form-element__control slds-input-has-fixed-addon">\n<input class="slds-input" type="text" id="input-id-' + id + '" placeholder="" />\n</div>\n</div>';
+  var widget = "";
+
+  switch(type) {
+    case 'select':
+      widget = '\r\n<div id="widget-' + id + '" class="slds-form-element">\r\n<label class="slds-form-element__label" for="selection-id-' + id + '">' + title + '</label>\r\n<div class="slds-form-element__control">\r\n<div class="slds-select_container">\r\n<select class="slds-select" id="input-id' + id + '">';
+      for (var i = 0; i < options.length; i++) {
+        var opt = options['list'][i];
+        var oValue;
+        var oText;
+        if (typeof opt == 'object') {
+          oValue = opt['value'];
+          oText = opt['text'];
+        } else {
+          oValue = opt;
+          oText = opt;
+        }
+        widget += '\r\n<option' + (value == oValue ? ' selected="selected"' : '') + ' value="' + oValue + '">' + oText + '</option>';
+      }
+      widget += '\r\n</select>\r\n</div>\r\n</div>\r\n</div>';
+      break;
+
+    default: // input
+      widget = '\r\n<div id="widget-' + id + '" class="slds-form-element" style="margin-bottom:10px;">\r\n<label class="slds-form-element__label" for="input-id-' + id + '">' + title + '</label>\r\n<div class="slds-form-element__control slds-input-has-fixed-addon">\r\n<input class="slds-input" type="text" id="input-id-' + id + '" placeholder="" />\r\n</div>\r\n</div>';
+      break;
+  }
+
   $('#workspace-container').append(widget);
 
   $('#input-id-'+id).data({'id': id}).val(value).change(function() {
@@ -100,20 +115,22 @@ function addWidget(id, name, value) {
 }
 
 sdk.getData(function (data) {
+  // get state data
   params = data['params'];
-  if (typeof params == 'undefined') params = {};
   ampscript = data['ampscript'];
-  if (typeof ampscript == 'undefined') ampscript = "";
   html = data['html'];
-  if (typeof html == 'undefined') html = "";
   preview_html = data['preview_html'];
+
+  // default state data
+  if (typeof params == 'undefined') params = {};
+  if (typeof ampscript == 'undefined') ampscript = "";
+  if (typeof html == 'undefined') html = "";
   if (typeof preview_html == 'undefined') preview_html = "";
-  // alert(getParams());
 
   // add the widgets to the page
   $('#workspace-container').html('');
   for (const param in params) {
-    addWidget(params[param]['id'], params[param]['name'], params[param]['value']);
+    addWidget(params[param]['id'], params[param]['name'], params[param]['value'], params[param]['type'], JSON.parse(params[param]['options']));
   }
 
   // add the html and ampscript to the editor
@@ -140,15 +157,32 @@ sdk.getData(function (data) {
       for (var i = 1; i < ampArray.length; i++) {
         var a = ampArray[i];
 
+        // parse name
         var name = a.substring(0, a.indexOf(" "));
 
+        // parse id
         var id = name.toLowerCase();
 
+        // parse value
         var vStart = a.substring(a.indexOf('"') + 1);
-        var value = htmlUnescape(vStart.substring(0, vStart.indexOf('"')));
+        var vEnd = vStart.indexOf('"');
+        var value = htmlUnescape(vStart.substring(0, vEnd));
 
-        params[id] = {'id': id, 'name': name, 'value': value};
-        addWidget(id, name, value);
+        // parse type and options
+        var paramType = 'input';
+        var comment = "";
+        var options = {};
+        var cStart = vStart.substring(vEnd);
+        var hasComment = indexOf("/* {");
+        if (hasComment >= 0) {
+          var comment = cStart.substring(hasComment, indexOf("*/")).trim();
+          var options = JSON.parse(comment);
+          var pType = options['type'];
+          if (typeof pType != 'undefined') paramType = pType.toLowerCase();
+        }
+
+        params[id] = {'id': id, 'name': name, 'value': value, 'type': paramType, 'options', comment};
+        addWidget(id, name, value, type, options);
       }
 
       html = data.substring(paramTextEnd + 26);
