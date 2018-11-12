bff217b8b4cce0aec1461c1c79767fb6df6b0a8b
bkaplan-github <43967620+bkaplan-github@users.noreply.github.com>
2018-11-12 16:32:49 -0500

    Add files via upload

--- dist/main.js
+++ dist/main.js
@@ -176,7 +176,7 @@ sdk.getData(function (data) {
         var cStart = extra.indexOf("/* {");
         if (cStart >= 0) {
           var cEnd = extra.indexOf("*/");
-          var comment = cStart.substring(cStart + 4, cEnd).trim();
+          var comment = extra.substring(cStart + 4, cEnd).trim();
           var options = JSON.parse(comment);
           var pType = options['type'];
           if (typeof pType != 'undefined') paramType = pType.toLowerCase();
