267e60629ade86548180f6525390b337c29e5f53
bkaplan-github <43967620+bkaplan-github@users.noreply.github.com>
2019-02-06 14:53:54 -0500

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,77 +1,255 @@
-require('../node_modules/@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css');
 
-var SDK = require('blocksdk');
-var sdk = new SDK();
+// var BlockSDK = require('blocksdk');
+// var sdk = new BlockSDK();
+var sdk = new window.sfdc.BlockSDK();
 
-var address, width, height, zoom, link, mapsKey;
+var params = {}; // parameter metadata
+var ampscript = ''; // ampscript variables
+var html = '' // html code without ampscript
+var preview_html = '' // preview html code
 
+/*
 function debounce (func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+*/
+
+function escapeRegExp(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 }
 
-function paintSettings () {
-	document.getElementById('text-input-id-0').value = mapsKey;
-	document.getElementById('text-input-id-1').value = address;
-	document.getElementById('slider-id-01').value = width;
-	document.getElementById('slider-id-02').value = height;
-	document.getElementById('slider-id-03').value = zoom;
-} 
-
-function paintSliderValues () {
-	document.getElementById('slider-id-01-val').innerHTML = document.getElementById('slider-id-01').value;
-	document.getElementById('slider-id-02-val').innerHTML = document.getElementById('slider-id-02').value;
-	document.getElementById('slider-id-03-val').innerHTML = document.getElementById('slider-id-03').value;
+function htmlEscape(str) {
+  return str
+    .replace(/&/g, '&amp;amp;')
+    .replace(/"/g, '&amp;quot;')
+    // .replace(/'/g, '&#39;')
+    // .replace(/\//g, '&#x2F;'); // forward slash
+    .replace(/</g, '&amp;lt;')
+    .replace(/>/g, '&amp;gt;');
 }
 
-function paintMap() {
-	mapsKey = document.getElementById('text-input-id-0').value;
-	address = document.getElementById('text-input-id-1').value;
-	width = document.getElementById('slider-id-01').value;
-	height = document.getElementById('slider-id-02').value;
-	zoom = document.getElementById('slider-id-03').value;
-	link = document.getElementById('text-input-id-2').value;
-	if (!address) {
-		return;
-	}
-	var url = 'https://maps.googleapis.com/maps/api/staticmap?center=' +
-		address.split(' ').join('+') + '&size=' + width + 'x' + height + '&zoom=' + zoom +
-		'&markers=' + address.split(' ').join('+') + '&key=' + mapsKey;
-	sdk.setContent('<a href="' + link + '"><img src="' + url + '" /></a>');
-	sdk.setData({
-		address: address,
-		width: width,
-		height: height,
-		zoom: zoom,
-		link: link,
-		mapsKey: mapsKey
-	});
-	localStorage.setItem('googlemapsapikeyforblock', mapsKey);
+function htmlUnescape(str){
+  return str
+    .replace(/&amp;quot;/g, '"')
+    // .replace(/&#39;/g, "'")
+    // .replace(/&#x2F;/g, '/'); // forward slash
+    .replace(/&amp;lt;/g, '<')
+    .replace(/&amp;gt;/g, '>')
+    .replace(/&amp;amp;/g, '&');
+}
+
+function ampEscape(str){
+  return str
+    .replace(/"/g, '""')
+    // .replace(/&#39;/g, "'")
+    // .replace(/&#x2F;/g, '/'); // forward slash
+    .replace(/&amp;lt;/g, '<')
+    .replace(/&amp;gt;/g, '>')
+    .replace(/&amp;amp;/g, '&');
+}
+
+function updateContent() {
+  var regex;
+  var fakehtml;
+
+  if (preview_html == "") fakehtml = html;
+  else fakehtml = preview_html;
+
+  ampscript = "";
+  if (Object.keys(params).length) {
+    ampscript = "%%[ /* PARAMETERS START */";
+    for (const param in params) {
+      var name = params[param]['name'];
+      var value = ampEscape(params[param]['value']);
+      var options = params[param]['options'];
+      ampscript += '\r\nSET @' + name + ' = TreatAsContent("' + value + '") /* ' + options + ' */';
+      regex = new RegExp(escapeRegExp("%%=v(@"+name+")=%%"), "gi");
+      fakehtml = fakehtml.replace(regex, value);
+    }
+    ampscript += "\r\n/* PARAMETERS END */ ]%%\r\n";
+  }
+
+  if (ampscript == "" && html == "") $("#editor").val("");
+  else $("#editor").val(ampscript+"\r\n"+html);
+
+  sdk.setData({'params': params, 'ampscript': ampscript, 'html': html, 'preview_html': preview_html});
+  sdk.setSuperContent(fakehtml);
+  sdk.setContent(ampscript+"\r\n"+html);
+}
+
+function addWidget(id, name, value, type, options) {
+  var regex = new RegExp(escapeRegExp("_"), "gi");
+  var title = name.replace(regex, " ");
+
+  // set the description
+  var description = "";
+  var desc = options["description"];
+  if (typeof desc != 'undefined') description = desc;
+
+  var widget = "";
+
+  switch(type) {
+    case 'selection':
+      widget = '\r\n<div id="widget-' + id + '" class="slds-form-element" title="' + description + '">\r\n<label class="slds-form-element__label" for="selection-id-' + id + '">' + title + '</label>\r\n<div class="slds-form-element__control">\r\n<div class="slds-select_container">\r\n<select class="slds-select" id="input-id-' + id + '">';
+      var olist = options['options'];
+
+      for (var i = 0; i < olist.length; i++) {
+        var opt = olist[i];
+        var oValue;
+        var oText;
+        if (typeof opt == 'object') {
+          oValue = opt['value'];
+          oText = opt['text'];
+        } else {
+          oValue = opt;
+          oText = opt;
+        }
+        widget += '\r\n<option' + (value == oValue ? ' selected="selected"' : '') + ' value="' + oValue + '">' + oText + '</option>';
+      }
+      widget += '\r\n</select>\r\n</div>\r\n</div>\r\n</div>';
+
+      $('#workspace-container').append(widget);
+
+      $('#input-id-'+id).data({'id': id}).val(value).change(function() {
+        var id = $(this).data('id');
+        var value = $(this).val();
+        params[id]['value'] = value;
+        updateContent();
+      });
+
+      break;
+
+    case 'slider':
+      var min = options["min"];
+      var max = options["max"];
+
+      widget = '\r\n<div id="widget-' + id + '" class="slds-form-element" title="' + description + '">\r\n<label class="slds-form-element__label" for="slider-id-' + id + '">\r\n<span class="slds-slider-label">\r\n<span class="slds-slider-label__label">' + title + '</span>\r\n</span>\r\n</label>\r\n<div class="slds-form-element__control">\r\n<div class="slds-slider">\r\n<input type="range" id="slider-id-' + id + '" class="slds-slider__range" value="' + value + '" min="' + min + '" max="' + max + '" />\r\n<span class="slds-slider__value" id="slider-num-id-' + id + '" aria-hidden="true">' + value + '</span>\r\n</div>\r\n</div>\r\n</div>';
+
+      $('#workspace-container').append(widget);
+
+      $('#slider-id-'+id).data({'id': id, 'prev_val': value}).val(value).mousemove(function() {
+        var id = $(this).data('id');
+        var value = $(this).val();
+        var prev_value = $(this).data('prev_val');
+        if (value != prev_value) {
+          $(this).data('prev_val', value);
+          $('#slider-num-id-'+id).html(value);
+          params[id]['value'] = value;
+          updateContent();
+        }
+      });
+
+      break;
+
+    default: // input
+      widget = '\r\n<div id="widget-' + id + '" class="slds-form-element" style="margin-bottom:10px;" title="' + description + '">\r\n<label class="slds-form-element__label" for="input-id-' + id + '">' + title + '</label>\r\n<div class="slds-form-element__control slds-input-has-fixed-addon">\r\n<input class="slds-input" type="text" id="input-id-' + id + '" placeholder="" />\r\n</div>\r\n</div>';
+
+      $('#workspace-container').append(widget);
+
+      $('#input-id-'+id).data({'id': id}).val(value).change(function() {
+        var id = $(this).data('id');
+        var value = $(this).val();
+        params[id]['value'] = value;
+        updateContent();
+      });
+
+      break;
+  }
 }
 
 sdk.getData(function (data) {
-	address = data.address || '';
-	width = data.width || 400;
-	height = data.height || 300;
-	zoom = data.zoom || 15;
-	link = data.link || '';
-	mapsKey = data.mapsKey || localStorage.getItem('googlemapsapikeyforblock');
-	paintSettings();
-	paintSliderValues();
-	paintMap();
-});
+  // get state data
+  params = data['params'];
+  ampscript = data['ampscript'];
+  html = data['html'];
+  preview_html = data['preview_html'];
 
-document.getElementById('workspace').addEventListener("input", function () {
-	debounce(paintMap, 500)();
-	paintSliderValues();
+  // default state data
+  if (typeof params == 'undefined') params = {};
+  if (typeof ampscript == 'undefined') ampscript = "";
+  if (typeof html == 'undefined') html = "";
+  if (typeof preview_html == 'undefined') preview_html = "";
+
+  // add the widgets to the page
+  $('#workspace-container').html('');
+  for (const param in params) {
+    addWidget(params[param]['id'], params[param]['name'], params[param]['value'], params[param]['type'], JSON.parse(params[param]['options']));
+  }
+
+  // add the html and ampscript to the editor
+  $("#editor").val(ampscript+"\r\n"+html);
+
+  // add the html to the preview
+  $('#preview').val(preview_html);
+
+  // update the editor
+  $("#editor").change(function() { // keyup()
+    var data = $(this).val();
+
+    params = {};
+    $('#workspace-container').html('');
+
+    // parse out the parameters
+    var paramTextStart = data.indexOf("%%[ /* PARAMETERS START */");
+    if (paramTextStart < 0) {
+      html = data;
+    }
+    else {
+      var paramTextEnd = data.indexOf("/* PARAMETERS END */ ]%%");
+      var amp = data.substring(paramTextStart, paramTextEnd);
+      var ampArray = amp.split("SET @");
+      for (var i = 1; i < ampArray.length; i++) {
+        var a = ampArray[i];
+
+        // parse name
+        var name = a.substring(0, a.indexOf(" "));
+
+        // parse id
+        var id = name.toLowerCase();
+
+        // parse value
+        var vStart = a.substring(a.indexOf('"') + 1);
+        var vEnd = vStart.indexOf('"');
+        var value = htmlUnescape(vStart.substring(0, vEnd));
+
+        // parse type and options
+        var paramType = 'input';
+        var comment = "";
+        var options = {};
+        var extra = vStart.substring(vEnd);
+        var cStart = extra.indexOf("/* {");
+        if (cStart >= 0) {
+          var cEnd = extra.indexOf("*/");
+          var comment = extra.substring(cStart + 3, cEnd).trim();
+          var options = JSON.parse(comment);
+          var pType = options['type'];
+          if (typeof pType != 'undefined') paramType = pType.toLowerCase();
+        }
+
+        params[id] = {'id': id, 'name': name, 'value': value, 'type': paramType, 'options': comment};
+        addWidget(id, name, value, paramType, options);
+      }
+
+      html = data.substring(paramTextEnd + 26);
+    }
+
+    updateContent();
+  });
+
+  // update the preview
+  $("#preview").change(function() { // keyup()
+    preview_html = $(this).val();
+    updateContent();
+  });
 });
