be3a1d762d80f8460d54e26d38befc8f769d2adb
bkaplan-github <43967620+bkaplan-github@users.noreply.github.com>
2019-08-09 14:59:00 -0400

    Add files via upload

--- dist/main.js
+++ dist/main.js
@@ -676,10 +676,10 @@ function addWidget(id, label, value, locked, type, tac, options) {
         var top = e.pageY - p.top;
         var width = $(this).width();
         var height = $(this).height();
-        var p_saturation =  Math.floor((left / width) * 100);
-        var p_value =  100 - Math.floor((top / height) * 100);
+        var p_saturation =  (Math.floor(left / width) * 100);
+        var p_value =  100 - (Math.floor(top / height) * 100);
 
-        console.log(" width:" + width + " height:" + height + " offX:" + p.left + " offY:" + p.top + " pageX:" + e.pageX + " pageY:" + e.pageY + " left:" + left + " top:" + top + " s:" + p_saturation + " v:" + p_value);
+        console.log(" width:" + width + " height:" + height + " offX:" + p.left + " offY:" + p.top + " pageX:" + e.pageX + " pageY:" + e.pageY + " left:" + left + " top:" + top + " px:" + (left / width) + " py:" + (top / height) + " s:" + p_saturation + " v:" + p_value);
 
         var widget = $('#widget-'+id);
 
